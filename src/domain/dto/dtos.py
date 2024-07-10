333d7b7e624db600398d5004d7c22d3beb77166e
DayanaIngrid <dayanaingrid.040@gmail.com>
2024-07-10 17:33:11 -0300

    DTO atualizado

--- src/domain/dto/dtos.py
+++ src/domain/dto/dtos.py
@@ -1,9 +1,6 @@
 from datetime import datetime
 from typing import Optional
-
 from pydantic import BaseModel
-
-
 class ProductDTO(BaseModel):
     id: int
     nome: str
@@ -11,13 +8,11 @@ class ProductDTO(BaseModel):
     preco: float
     estoque: int
 
-
 class ProductCreateDTO(BaseModel):
     nome: str
     descricao: str
     preco: float
     estoque: int
-
 
 class ProductUpdateDTO(BaseModel):
     nome: Optional[str] = None
@@ -25,11 +20,9 @@ class ProductUpdateDTO(BaseModel):
     preco: Optional[float] = None
     estoque: Optional[int] = None
 
-
 class TokenResponse(BaseModel):
     access_token: str
     expires_at: datetime
-
 
 class UserTokenDataResponse(BaseModel):
     expires_at: datetime
